0803632bbe51718a644360e23f84c3129153b138
kngDaveed <davidgodfrey365@gmail.com>
2025-06-22 05:56:21 +0100

    Minify

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -36,7 +36,7 @@ function History({ history, setHistory }) {
       {visibleLinks.map((item, index) => (
         <div
           key={`${item.shortUrl}-${index}`}
-          className="flex justify-between items-center border-b py-2"
+          className="flex justify-between items-center border border-gray-200 rounded-md mb-2 p-2"
         >
           <div className="w-3/4">
             <p className="truncate text-sm text-gray-700">
@@ -53,7 +53,7 @@ function History({ history, setHistory }) {
           </div>
           <button
             onClick={() => deleteLink(item.shortUrl)}
-            className="text-red-500 text-xs ml-2"
+            className="bg-red-500 text-white px-4 py-3 rounded-md hover:bg-red-600 transition text-xs ml-2"
           >
             Delete
           </button>
