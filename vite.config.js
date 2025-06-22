528978a36d2390ee8bffbfb360981e38fd9df112
kngDaveed <davidgodfrey365@gmail.com>
2025-06-22 04:21:46 +0100

    Minify😩

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import htmlMeta from 'vite-plugin-html-meta';
+// import htmlMeta from 'vite-plugin-html-meta';
 
 export default defineConfig({
   plugins: [
     react(),
-    htmlMeta({
-      title: 'Minify | Free URL Shortener',
-      description: 'Shorten URLs with ease. Clean, fast, and copy-ready. Built with React + Vite.',
-      image: 'https://miniphy.vercel.app/og-preview.png',
-      url: 'https://miniphy.vercel.app',
-    }),
+    // htmlMeta({
+    //   title: 'Minify | Free URL Shortener',
+    //   description: 'Shorten URLs with ease. Clean, fast, and copy-ready. Built with React + Vite.',
+    //   image: 'https://miniphy.vercel.app/og-preview.png',
+    //   url: 'https://miniphy.vercel.app',
+    // }),
   ],
   base: process.env.VITE_BASE_PATH || '/',
   build: {
